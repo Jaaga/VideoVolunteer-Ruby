91b949298e5e8e89e17f4b7d2467000e16d0bc86
Brice <bgatelet@gmail.com>
2015-02-27 16:28:18 +0530

    Further modified search feature.

--- app.rb
+++ app.rb
@@ -141,42 +141,45 @@ get '/search' do
   haml :search
 end
 
+# Standard searches will be here.
 post '/search' do
-  arr = ['uid', 'state', 'district', 'mentor', 'cc_name', 'iu_theme', 'subcategory', 'description', 'story_type', 'shoot_plan', 'story_rating',  'editor', 'edit_status', 'payment_status', 'description', 'folder_title', 'review_notes', 'edited_video_rating', 'youtube_url', 'video_title', 'subtitle_info', 'subtheme', 'project', 'reviewer_name', 'editor_changes_needed', 'cc_feedback', 'publishing_suggestions', 'final_video_rating', 'stalin_notes', 'video_type',   'impact_possible', 'target_official', 'target_official_email', 'target_official_phone', 'desired_change', 'impact_plan', 'impact_followup', 'impact_followup_notes', 'impact_uid', 'impact_process', 'impact_status', 'milestone', 'impact_time', 'collaborations', 'people_involved', 'people_impacted', 'villages_impacted', 'impact_production_status', 'impact_review', 'payment_approved', 'impact_reviewer',  'screening_done', 'screening_headcount', 'screening_notes', 'official_involved', 'officials_at_screening_number', 'officials_at_screening', 'official_screening_notes', 'flag_notes', 'note']
-  dates = ['story_pitch_date', 'received_cc_date', 'edit_in_goa_date', 'state_rough_cut_date', 'goa_rough_cut_date', 'raw_footage_review_date', 'backup_received_date', 'state_edit_date', 'edit_received_date', 'rough_cut_edit_date', 'review_date', 'finalized_date', 'youtube_date', 'iu_publish_date', 'impact_date', 'impact_approval_date', 'flag_date']
-
-  data = Array.new
-  index = Array.new
   search = Array.new
 
-  # if params[:no_video] == "on"
-  #   search.push("story_pitch_date NOT NULL AND backup_received_date IS NULL")
-  # end
-  #
-  # if params[:no_publish] == "on"
-  #   search.push("rough_cut_edit_date NOT NULL AND youtube_date IS NULL")
-  # end
-  #
-  # if params[:flag] == "on"
-  #   data.push("'priority'")
-  #   index.push("flag")
-  # end
-  #
-  # arr.each do |x|
-  #   if !params[:"#{x}"].blank?
-  #     data.push("'#{params[:"#{x}"]}'")
-  #     index.push(x)
-  #   end
-  # end
-  #
-  # index.each_with_index do |v, i|
-  #   search.push("#{v} = #{data[i]}")
-  # end
-  #
-  # search = search.join(" AND ")
+  if params[:no_video] == "on"
+    search.push("story_pitch_date NOT NULL AND backup_received_date IS NULL")
+  end
 
-  # @track = Tracker.where("#{search}")
-  @track = Tracker.where("#{params[:column]} #{params[:operator]} #{params[:input]}")
+  if params[:no_publish] == "on"
+    search.push("rough_cut_edit_date NOT NULL AND youtube_date IS NULL")
+  end
+
+  search = search.join(" AND ")
+
+  @track = Tracker.where(search)
+  @title = 'Search Results'
+
+  haml :results
+end
+
+# Custom queries come through here.
+post '/search/custom' do
+  search = Array.new
+
+  # Goes through each query row and checks if data is entered for the search query.
+  (0..9).each do |x|
+    if !params[:"column_#{x}"].blank? && !params[:"input_#{x}"].blank?
+      search.push("#{params[:"column_#{x}"]} #{params[:"operator_#{x}"]} '#{params[:"input_#{x}"]}'")
+    end
+
+    # Only add chain if input was filled in and column was chosen.
+    if !params[:"chain_#{x}"].blank? && !params[:"column_#{x}"].blank? && !params[:"input_#{x}"].blank?
+      search.push(params[:"chain_#{x}"])
+    end
+  end
+
+  search = search.join(' ')
+
+  @track = Tracker.where(search)
   @title = 'Search Results'
 
   haml :results
@@ -272,7 +275,7 @@ get '/unflag/:uid' do
 end
 
 
-# Making notes on correspondents
+# Making notes on stories/videos
 
 get '/note' do
   haml :note
