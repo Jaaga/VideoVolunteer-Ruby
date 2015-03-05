1d296f2aab54e067ad8a0a83e576f18de34ba39e
Brice <bgatelet@gmail.com>
2015-03-05 13:21:26 +0530

    Migrate

--- db/schema.rb
+++ db/schema.rb
@@ -102,7 +102,6 @@ ActiveRecord::Schema.define(version: 20150226064312) do
     t.string   "people_impacted"
     t.string   "villages_impacted"
     t.string   "impact_review"
-    t.string   "payment_approved"
     t.string   "impact_reviewer"
     t.date     "impact_date"
     t.date     "impact_approval_date"
