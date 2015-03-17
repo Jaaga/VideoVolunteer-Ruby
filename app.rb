388bf977d1cc922ef861fab791d073391f32a536
Brice <bgatelet@gmail.com>
2015-03-17 12:29:52 +0530

    Added stats

--- app.rb
+++ app.rb
@@ -64,6 +64,14 @@ get '/' do
   @rough = Tracker.where("edit_received_in_goa_date IS NOT NULL AND youtube_date IS NULL")
   # Number of videos with an impact achieved
   @impact = Tracker.where("impact_achieved = ?", 'yes')
+  # Videos whose rough cuts have arrived from a state office and need to be cleaned, reviewed and uploaded
+  @goa_bank = Tracker.where("edit_received_in_goa_date IS NOT NULL")
+  # Total approved videos whose edits haven't reached goa
+  @state_bank = Tracker.where("edit_received_in_goa_date IS NULL AND rough_cut_edit_date IS NOT NULL")
+  # Videos on hold
+  @hold = Tracker.where("proceed_with_edit_and_payment = ?", 'On hold')
+  # Videos uploaded to youtube
+  @youtube = Tracker.where("youtube_date IS NOT NULL")
 
   haml :index
 end
